c8b65ed70eeaf1dc2a7bff82dac52a132133887c
RazorsHyper <48493750+RazorsHyper@users.noreply.github.com>
2019-04-03 11:36:06 +0200

    Update and rename config.example.json to config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 {
-  "token": "your bot token",
-  "mailGuildId": "id of the modmail inbox server",
-  "mainGuildId": "id of the main server where users will DM the bot",
+  "token": "NTUzOTQ4MzEyMjczODEzNTA0.XKR3fw.e82k-UHE4PDNtAm85_GzA-nJc44",
+  "mailGuildId": "562714657333444618",
+  "mainGuildId": "562337999573221377",
   "logChannelId": "id of the channel on the inbox server where notifications of new logs etc. will be posted",
 
   "status": "Message me for help!",
